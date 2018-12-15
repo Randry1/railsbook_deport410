73c95169fe14e757a364219a5917865b790c3950
Randry1 <revinandry@gmail.com>
2018-12-15 21:24:13 +0700

    Добавлен конторллер Сторе, и scss файл к нему, передалан аликатион

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products do
     get 'upgrade', on: :collection
   end
 
+  root to: 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
